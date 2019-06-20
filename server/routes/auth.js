8333c8c56c28a03cde1cd90b8b763b1410808db4
Suyash Singh <suyashsingh234@gmail.com>
2019-06-20 14:46:35 +0530

    fixed add button and backend code

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,7 +1,7 @@
 const express=require('express');
 const router=express.Router();
 const passport=require('passport');
-const bcrypt=require('bcrypt');
+const bcrypt=require('bcryptjs');
 const collection=require('../database/models.js').collection;
 
 module.exports=function(passport){
